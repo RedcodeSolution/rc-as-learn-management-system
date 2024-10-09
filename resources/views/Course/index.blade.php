648cb7e013e9cf15f9ecac7b0c2210f6a223e14f
Kavindu Hansaka <127814865+K4VI12@users.noreply.github.com>
2024-10-09 18:08:43 +0530

    changed

--- resources/views/Course/index.blade.php
+++ resources/views/Course/index.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex justify-end items-center">
             <a href="/course/create" class="text-black px-4 py-2 rounded">
-                Create new Course
+                Create
             </a>
         </div>
     </x-slot>
@@ -14,18 +14,27 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
 
-            <div class="text-gray-900" style="font-size: 50px; background-color: #BF4D4D; padding: 40px; width: 100%; height: 250px; display: flex; align-items: center;">
-                    {{ __("HTML") }}
+                <div class="text-gray-900" style="font-size: 50px; background-color: #BF4D4D; padding: 40px; width: 100%; height: 250px; display: flex; align-items: center;">
+                <a href="">
+                        {{ __("HTML") }}
+                        {{ __("full course 9hr conducted by john 50 videos") }}
+                    </a>
                 </div>
 
 
                 <div class="text-gray-900" style="font-size: 50px; background-color: #ECDFCC; padding: 40px; width: 100%; height: 250px; display: flex; align-items: center;">
-                    {{ __("CSS") }}
+                <a href="">
+                        {{ __("CSS") }}
+                        {{ __("full course 9hr conducted by john 50 videos") }}
+                    </a>
                 </div>
 
 
                 <div class="text-gray-900" style="font-size: 50px; background-color: #598A8D; padding: 40px; width: 100%; height: 250px; display: flex; align-items: center;">
-                    {{ __("JAVASCRIPT") }}
+                    <a href="">
+                        {{ __("JAVASCRIPT") }}
+                        {{ __("full course 9hr conducted by john 50 videos") }}
+                    </a>
                 </div>
 
 
